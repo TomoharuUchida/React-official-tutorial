ac4cfa79765ff99a55e715074ccc278149b3b2fe
TomoharuUchida <tomou0711@gmail.com>
2022-04-23 20:26:49 +0900

    改良:renderSquareをハードコーディングからループの使用に修正

--- src/index.js
+++ src/index.js
@@ -18,35 +18,34 @@ class Board extends React.Component {
 
     
   renderSquare(i) {
-      return(
-        <Square
-            value={this.props.squares[i]}
-            onClick={() => this.props.onClick(i)}
-        />
-      )
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+        key={i}
+      />
+    )
   }
 
-    render() {
+  render() {
+    const rows = [0, 1, 2];
+    const cols = [0, 1, 2];
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows.map(row => {
+          return (
+            <div
+              className="board-row"
+              key={row}
+            >
+              {cols.map(col => this.renderSquare(row * 3 + col))}
+            </div>
+          );
+        })}
       </div>
+             
     );
-    }
+  }
 }
 
 class Game extends React.Component {
@@ -84,7 +83,6 @@ class Game extends React.Component {
             stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
-      console.log(history);
     }
     jumpTo(step) {
         this.setState({
